4568a2ac28b2af89afa71bbcfeb65a6cf80265fc
josesolis-06 <124844596+josesolis-06@users.noreply.github.com>
2023-02-09 03:00:08 +0000

    leccion 03 código

---
+++ listas.py
@@ -0,0 +1,15 @@
+#lista_del_mago = "patas de rana, lengua de rana, ojo de buey, ala de murcielago, "
+
+#print(lista_del_mago)
+
+lista = [1 , 2, 3, 4]
+
+lista_del_mago = ["patas de rana", "lengua de rana", "ojo de buey", "ala de murcielago"]
+
+#print(lista_del_mago)
+#print(lista_del_mago[0:2])
+#print(lista_del_mago[1:4])
+
+lista_del_mago.append("piel de serpiente")
+del lista_del_mago[0]
+print(lista_del_mago)
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 print("Hello")
+
+puntuacion = 6000
+
+kilómetros = 150
+
+mensaje = "Carlos tuvo una puntuacion de: %s puntos y recorrio %s kilómetros"
+
+print(mensaje % (puntuacion, kilómetros))
